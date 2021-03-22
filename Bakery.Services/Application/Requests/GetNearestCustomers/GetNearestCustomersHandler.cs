6693b9e0fe61c9892e6d6449e811a7fabe1a251e
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2021-03-22 20:57:57 +0700

    дописан GetNearestCustomersHandler

--- Bakery.Services/Application/Requests/GetNearestCustomers/GetNearestCustomersHandler.cs
+++ Bakery.Services/Application/Requests/GetNearestCustomers/GetNearestCustomersHandler.cs
@@ -1,9 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
-using AutoMapper.QueryableExtensions;
 using Bakery.Core;
 using Bakery.Services.Application.Models;
 using Bakery.Services.Application.Models.Customer;
@@ -15,7 +15,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Bakery.Services.Application.Requests.GetNearestCustomers
 {
-    public class GetNearestCustomersHandler : IRequestHandler<GetNearestCustomersRequest, Result<AddressableCustomerDto>>
+    public class GetNearestCustomersHandler : IRequestHandler<GetNearestCustomersRequest, Result<IEnumerable<AddressableCustomerDto>>>
     {
         private ICustomerService _customerService;
         private AppDbContext _db;
@@ -29,18 +29,40 @@ namespace Bakery.Services.Application.Requests.GetNearestCustomers
             _addressService = addressService;
         }
         
-        public async Task<Result<AddressableCustomerDto>> Handle(GetNearestCustomersRequest request, CancellationToken cancellationToken)
+        public async Task<Result<IEnumerable<AddressableCustomerDto>>> Handle(GetNearestCustomersRequest request, CancellationToken cancellationToken)
         {
             var location = new Location(){Latitude = request.Request.Latitude, Longitude = request.Request.Longitude};
             var addresses = await _addressService.GetNearest(location, request.Request.Distance);
-            var ids = addresses.Value.Select(x => x.CustomerId).ToHashSet();
+            var dictionary = addresses.Value
+                .GroupBy(x => x.CustomerId)
+                .ToDictionary(x => x.Key, g => g.ToList());
+            
             //todo: переместить в сервис  и разобраться с маппингами
-            var customers = await _db.Customer
-                .Where(l => ids.Contains(l.CustomerId))
-                .ToListAsync();
-            
-            
-            throw new NotImplementedException();
+            var customers = await _db.CustomerAddress.Include(x => x.Customer)
+                .Where(l => dictionary.ContainsKey(l.CustomerId))
+                .AsNoTracking()
+                .Select(x => new
+                {
+                    CustomerId = x.CustomerId, 
+                    CustomerName = x.Customer.CustomerName, 
+                    CustomerDescription = x.Customer.CustomerDescription, 
+                    DateStart = x.Customer.DateStart, 
+                    DateEnd = x.Customer.DateEnd, 
+                    AddressId = x.AddressId
+                })
+                .ToListAsync(cancellationToken);
+
+            var result = customers.Select(x => new AddressableCustomerDto()
+            {
+                CustomerId = x.CustomerId,
+                CustomerName = x.CustomerName,
+                CustomerDescription = null,
+                DateStart = null,
+                DateEnd = null,
+                Address = dictionary[x.CustomerId].FirstOrDefault(a => a.AddressId == x.AddressId)
+            });
+
+            return Result<IEnumerable<AddressableCustomerDto>>.Create(result);
         }
     }
 }
--- Bakery.Services/Application/Requests/GetNearestCustomers/GetNearestCustomersRequest.cs
+++ Bakery.Services/Application/Requests/GetNearestCustomers/GetNearestCustomersRequest.cs
@@ -1,10 +1,11 @@
+using System.Collections.Generic;
 using Bakery.Services.Application.Models;
 using Bakery.Services.Application.Models.Customer;
 using MediatR;
 
 namespace Bakery.Services.Application.Requests.GetNearestCustomers
 {
-    public class GetNearestCustomersRequest : IRequest<Result<AddressableCustomerDto>>
+    public class GetNearestCustomersRequest : IRequest<Result<IEnumerable<AddressableCustomerDto>>>
     {
         public Models.Customer.GetNearestCustomers Request { get; set; }
     }
